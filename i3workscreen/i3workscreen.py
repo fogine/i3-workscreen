67e75ce703e6dc9e3cb0e60cf079782724cc0c05
fogine <7884288+fogine@users.noreply.github.com>
2018-03-07 21:13:33 +0100

    implements #1 - toggling between mirrored & extended display modes

--- i3workscreen/i3workscreen.py
+++ i3workscreen/i3workscreen.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import argparse
 import os
 import sys
 import json
@@ -32,16 +33,27 @@ def get_outputs(display):
     """
     root = display.screen().root
     resources = root.xrandr_get_screen_resources()._data
+    primaryOutput = root.xrandr_get_output_primary().output
     outputs = []
 
     for output in resources['outputs']:
         _data = display.xrandr_get_output_info(output,
                 resources['config_timestamp'])._data
+        x = y = 0
+
+        if _data['crtc']:
+            crtcInfo = display.xrandr_get_crtc_info(_data['crtc'],
+                    resources['config_timestamp'])
+            x = crtcInfo.x
+            y = crtcInfo.y
 
         outputs.append({
+            'isPrimary': output == primaryOutput,
             'name': _data['name'],
             'connected': not _data['connection'],
-            'dissabled': not _data['crtc']
+            'dissabled': not _data['crtc'],
+            'x': x,
+            'y': y
         });
 
     return outputs
@@ -93,31 +105,73 @@ def createi3CmdString(outputName, workspaces):
         cmd += 'workspace {0}; move workspace to output {1}; '.format(workspace, outputName)
     return cmd
 
-def getBuiltInDisplayOutputName(outputs):
+def areDisplayOutputsCloned(outputs):
+    """returns True whether more than one display output points at the same
+    framebuffer address
+
+    :outputs: array
+    :returns: boolean
+
+    """
+    for index,output in enumerate(outputs):
+        if index > 0:
+            if output['x'] != outputs[index-1]['x'] or output['y'] != outputs[index-1]['y']:
+                return False
+
+    return True
+
+def getShellArgs():
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--toggle', action='store_true', help='Toggles between mirrored & extended display mode')
+    return parser.parse_args()
+
+def getCloneCandidate(outputs):
+    """
+    :returns single connected & enabled display output which will be cloned
+    """
+    for output in outputs:
+        if output['isPrimary']:
+            return output
+
+    return outputs[0]
+
+def getBuiltInDisplayOutput(outputs):
     """detects built-in laptop display output.
-    returns first active output name whose name starts either with `eDP` or `LVDS`
-    TODO: do you know a better method of detecting this
-    (other than manual configuration), let me know please!
+    returns first active output whose name starts either with `eDP` or `LVDS`
 
     :outputs: array<object>
     :returns: string
 
     """
+    #TODO: do you know a better method of detecting this
+    #(other than manual configuration), let me know please!
     for output in outputs:
         if output['name'].startswith('eDP') or output['name'].startswith('LVDS'):
-            return output['name']
+            return output
+    return None
 
 def main():
     # Check for extension
     if not d.has_extension('RANDR'):
         raise RuntimeError('server does not have the RANDR extension')
 
+    shellArgs=getShellArgs()
     #xrandr outputs (like HDMI-1, DP-1 etc..)
     outputs = get_outputs(d)
     #identifier of currently focues i3 workspace
     focusedWorkspace = i3.get_tree().find_focused().workspace().name
+    #tried to identify opitonal laptop display output
+    builtInOutput = getBuiltInDisplayOutput(outputs)
     config = get_config(CONFIG)
+    xrandr = {
+        'cloned': [],
+        'extended': [],
+        'laptop_disabled': [],
+        'other_disabled': [],
+        'disabled': []
+    }
     connectedOutputs = []
+    enabledOutputs = []
     #user configured list of workspace identifiers assigned to particular display output
     connectedOutputWorkspaces = []
     #i3 multi command string which will bind workspaces to correct display outputs
@@ -144,15 +198,20 @@ def main():
             if 'workspaces' in output:
                 connectedOutputWorkspaces.insert(index, output['workspaces'])
 
-    builtInDisplayOutput = getBuiltInDisplayOutputName(connectedOutputs)
+    outputCloneCandidate = getCloneCandidate(connectedOutputs)
 
     for index,output in enumerate(connectedOutputs):
         #TODO find out how to do the same thing with Xlib
         #witout spawning a shell process?
-        xrandrArgs = ['xrandr', '--output', output['name'], '--auto']
+        xrandr['extended'] += ['--output', output['name'], '--auto']
+        xrandr['cloned'] += xrandr['extended'][:]
+
+        if output['name'] != outputCloneCandidate['name']:
+            xrandr['cloned'] += ['--same-as', outputCloneCandidate['name']]
+        # xrandr = ['xrandr', '--output', output['name'], '--auto']
         if 'xrandr' in output:
-            xrandrArgs = xrandrArgs + output['xrandr']
-        subprocess.run(args=xrandrArgs, check=True)
+            xrandr['extended'] += output['xrandr']
+        # subprocess.run(args=xrandr, check=True)
 
         try:
             nextOutput = connectedOutputs[index + 1]
@@ -168,10 +227,21 @@ def main():
            (output['connected'] and not len(output['workspaces']) and config['disableEmptyOutputs'])):
             #TODO find out how to do the same thing with Xlib
             #witout spawning a shell process?
-            xrandrArgs = ['xrandr', '--output', output['name'], '--auto']
-            xrandrArgs = ['xrandr', '--output', output['name'], '--off']
-            subprocess.run(args=xrandrArgs, check=True)
+            xrandr['disabled'] += ['--output', output['name'], '--off']
 
+
+    args = ['xrandr']
+
+    if shellArgs.toggle:
+        if len(connectedOutputs) > 1 and not areDisplayOutputsCloned(connectedOutputs):
+            print(xrandr['cloned'])
+            args += xrandr['cloned'] + xrandr['disabled']
+        elif len(connectedOutputs) > 1:
+            args += xrandr['extended'] + xrandr['disabled']
+    else:
+        args += xrandr['extended'] + xrandr['disabled']
+
+    subprocess.run(args=args, check=True) #xrandr
     i3.command(i3cmd)
     i3.command('workspace {0}'.format(focusedWorkspace))
 
@@ -182,5 +252,6 @@ if __name__ == '__main__':
     try:
         main()
     except Exception as e:
-        logger(str(e))
+        # logger(str(e))
+        raise e
         sys.exit(1);
